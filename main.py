26b01d84d23ad5119db4ffd485051d91609bcaf3
ValitosSensei <89963241+ValitosSensei@users.noreply.github.com>
2025-04-10 12:09:31 +0300

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ BOT_TOKEN = os.getenv("BOT_TOKEN")
 DATABASE_URL = os.getenv("DATABASE_URL")
 
 bot = telebot.TeleBot(BOT_TOKEN)
-ADMIN_IDS = [123456789]  # заміни на свій Telegram ID
+ADMIN_IDS = [385386611]  # заміни на свій Telegram ID
 
 # Підключення до бази
 conn = psycopg2.connect(DATABASE_URL, sslmode='require')
